10aa540b1d64d5e2561421c2ba5044a6dd74c5ee
민수민 <soo@minsumin-ui-MacBookPro.local>
2025-09-04 00:25:49 +0900

    feat: tailwind 기본 설정 및 라이브러리 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     tanstackRouter({
       routesDirectory: "src/routes", // __root.tsx가 있는 폴더
       generatedRouteTree: "src/routeTree.gen.ts",
